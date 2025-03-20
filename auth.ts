c11717741e20c6937c7b2fe36fbb5326030e88a9
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-03-20 16:23:39 +0100

    Callbacks setup
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- auth.ts
+++ auth.ts
@@ -1,9 +1,45 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { authConfig } from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./lib/prisma"
- 
+import { getUserById } from "@/data/user"
+import { UserRole } from "@prisma/client"
+
+declare module "next-auth" {
+    interface Session {
+      user: {
+        role: UserRole;
+      } & DefaultSession["user"]
+    }
+  }
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            
+            if (token.sub && session.user) {
+                session.user.id = token.sub
+            }
+
+            if (token.role && session.user) {
+                session.user.role = token.role as UserRole;
+            }
+
+            return session
+        },
+        async jwt({ token }) {
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+            
+
+            if (!existingUser) return token;
+            
+            token.role = existingUser.role;
+
+            return token
+        },
+    },
     adapter: PrismaAdapter(prisma),
     // This strategy stores session data in a JWT rather than in a database due to edge compatability.
     session: {
